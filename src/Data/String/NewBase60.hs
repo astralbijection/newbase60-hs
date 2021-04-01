08da36c9ac2df89cc3424f401af3c1724ff169c4
Astrid Yu <astrid@astrid.tech>
2021-03-31 20:16:44 -0700

    Implement sxgToNum

--- src/Data/String/NewBase60.hs
+++ src/Data/String/NewBase60.hs
@@ -1,9 +1,13 @@
 module Data.String.NewBase60
   ( numToSxg,
+    sxgToNum,
   )
 where
 
 import Data.Array (Array, listArray, (!))
+import Data.Char (ord)
+import Data.List (foldl')
+import Data.Maybe (catMaybes, mapMaybe)
 
 lookupArray :: Array Integer Char
 lookupArray = listArray (0, 59) "0123456789ABCDEFGHJKLMNPQRSTUVWXYZ_abcdefghijkmnopqrstuvwxyz"
@@ -17,3 +21,20 @@ numToSxg n = convert "" n
       let d = n `mod` 60
           ch = lookupArray ! d
        in convert (ch : s) (n `div` 60)
+
+sxgToNum :: String -> Integer
+sxgToNum chs =
+  foldl' (\n digit -> n * 60 + digit) (0 :: Integer) $
+    map toInteger $ mapMaybe convertDigit chs
+  where
+    convertDigit c
+      | '0' <= c && c <= '9' = Just (ord c - ord '0')
+      | 'A' <= c && c <= 'H' = Just (ord c - ord 'A' + 10)
+      | 'J' <= c && c <= 'N' = Just (ord c - ord 'J' + 18)
+      | 'P' <= c && c <= 'Z' = Just (ord c - ord 'P' + 23)
+      | c == '_' = Just 34 -- typo capital I, lowercase l to 1
+      | 'a' <= c && c <= 'k' = Just (ord c - ord 'a' + 35)
+      | 'm' <= c && c <= 'z' = Just (ord c - ord 'm' + 46)
+      | c == 'I' || c == 'l' = Just 1 -- error correct typo capital O to 0
+      | c == 'O' = Just 0 -- skip invalid chars
+      | otherwise = Nothing
